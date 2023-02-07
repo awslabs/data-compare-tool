756f855e30812d1cb70227e5186934431a248609
rajkag <rajkag@amazon.com>
2023-02-07 12:42:10 +0530

    recommendation api refactoring

--- src/main/java/com/datavalidationtool/controller/RecommendationController.java
+++ src/main/java/com/datavalidationtool/controller/RecommendationController.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.*;
 @RestController
-@RequestMapping("/recommendation/api")
+@RequestMapping("/dvt")
 public class RecommendationController {
 
     @Autowired
@@ -32,97 +32,15 @@ public class RecommendationController {
     @Autowired
     private ExcelDataService excelDataService;
 
-    //http://localhost:8080/recommendation/api/test
-    @GetMapping("/test")
+    //http://localhost:8080/recommendation/test
+    @GetMapping("/recommendation/test")
     public String recommendationApiTest() {
 
         return recommendationService.recommendationApiTest();
     }
 
-    //http://localhost:8080/recommendation/api/schema
-    @GetMapping("/schema")
-    public List<String> getDbSchemaDetails() {
-
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432, "postgres",
-                null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        return recommendationService.getDbSchemaDetails(databaseInfo);
-    }
-
-
-    //http://localhost:8080/recommendation/api/table?schemaName=test_schema_2
-    @GetMapping("/table")
-    public List<String> getSchemaTableDetails(@RequestParam String schemaName) throws Exception {
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432, "postgres",
-                null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        //String schemaName="test_schema_2";
-        return recommendationService.getSchemaTableDetails(databaseInfo, schemaName);
-    }
-
-    //http://localhost:8080/recommendation/api/run-details/optional?sourceHostName=localhost&targetHostName=localhost&databaseName=postgres
-    //http://localhost:8080/recommendation/api/run-details/optional?sourceHostName=localhost&targetHostName=localhost&databaseName=ttp
-    @GetMapping("/run-details/optional")
-    public List<RunDetails> getRunDetailsWithOptional(
-            @RequestParam Optional<String> sourceHostName,
-            @RequestParam Optional<String> targetHostName,
-            @RequestParam Optional<String> databaseName,
-            @RequestParam Optional<String> schemaName,
-            @RequestParam Optional<String> tableName,
-            @RequestParam Optional<Integer> schemaRun,
-            @RequestParam Optional<Integer> tableRun
-    ) throws Exception {
-
-        RunDetails inputRunDetails = new RunDetails();
-        if (sourceHostName.isPresent()) {
-            inputRunDetails.setSourceHostName(sourceHostName.get());
-        }
-        if (targetHostName.isPresent()) {
-            inputRunDetails.setTargetHostName(targetHostName.get());
-        }
-        if (databaseName.isPresent()) {
-            inputRunDetails.setDatabaseName(databaseName.get());
-        }
-        if (schemaName.isPresent()) {
-            inputRunDetails.setSchemaName(schemaName.get());
-        }
-        if (tableName.isPresent()) {
-            inputRunDetails.setTableName(tableName.get());
-        }
-        if (schemaRun.isPresent()) {
-            inputRunDetails.setSchemaRun(schemaRun.get());
-        }
-        if (tableRun.isPresent()) {
-            inputRunDetails.setTableRun(tableRun.get());
-        }
-
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        return recommendationService.getRunDetailsWithOptional(inputRunDetails, databaseInfo);
-    }
-
-
-    //http://localhost:8080/recommendation/api/host-run-details?sourceHostName=localhost
-
-    @GetMapping("/host-run-details")
-    public List<RunDetails> getHostRunDetails(@RequestParam String sourceHostName) throws Exception {
-
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        List<RunDetails> runDetails = recommendationService.getHostRunDetails(sourceHostName, databaseInfo);
-
-        return runDetails;
-    }
-
-
-    //http://localhost:8080/recommendation/api/host-run-details/selection
-    @GetMapping("/host-run-details/selection")
+    //http://localhost:8080/dvt/recommendation/recommendation-selection?sourceHostName=localhost
+    @GetMapping("/recommendation/recommendation-selection")
     public Object getHostRunDetailsSelectionResponse() throws Exception {
 
         DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
@@ -137,87 +55,11 @@ public class RecommendationController {
         return runDetailBeans;
     }
 
-    //http://localhost:8080/recommendation/api/host-run-details/custom?sourceHostName=localhost
-    @GetMapping("/host-run-details/custom")
-    public Object getHostRunDetailsCustomResponse(@RequestParam String sourceHostName) throws Exception {
-
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        List<RunDetails> runDetails = recommendationService.getHostRunDetails(sourceHostName, databaseInfo);
-
-        if (!runDetails.isEmpty()) {
-            return recommendationService.getRunDetailsSelectionResponse(runDetails);
-        }
-        return runDetails;
-    }
-
-    //http://localhost:8080/recommendation/api/source-target/records?sourceHostName=localhost&targetHostName=localhost&databaseName=ttp&schemaName=ops$ora&tableName=ppt12&schemaRun=1&tableRun=2
-    @PostMapping(path = "/source-target/records",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Integer> getValIdFromValidationTable(@RequestBody RunDetails inputRunDetails) throws Exception {
-
-
-        DatabaseInfo runTableDbInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        List<RunDetails> runDetailsList = recommendationService.getRunDetailsWithOptional(inputRunDetails, runTableDbInfo);
-        Optional<RunDetails> runDetails = runDetailsList.stream().findFirst();
-
-
-        List<Integer> sourceValIdFromValidationTable = new ArrayList<>();
-        if(runDetails.isPresent()) {
-            DatabaseInfo valTableDbInfo = new DatabaseInfo("localhost", 5432,
-                    "ttp", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                    true, "/certs/", "changeit");
-
-            String validationTableName = runDetails.get().getTableName() + "_val";
-            sourceValIdFromValidationTable = recommendationService.getValIdFromValidationTable(runDetails.get(), valTableDbInfo, validationTableName);
-        }
-        return sourceValIdFromValidationTable;
-
-    }
-
-
-
-    //http://localhost:8080/recommendation/api/source-target/entire-records
-    // Pass below as post request body
-    // ?sourceHostName=localhost&targetHostName=localhost&databaseName=ttp&schemaName=ops$ora&tableName=ppt12&schemaRun=1&tableRun=2
-    @PostMapping(path = "/source-target/entire-records",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Map<String, Object>> getEntireValidationTable(@RequestBody RunDetails inputRunDetails) throws Exception {
-
-
-        DatabaseInfo runTableDbInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        List<RunDetails> runDetailsList = recommendationService.getRunDetailsWithOptional(inputRunDetails, runTableDbInfo);
-        Optional<RunDetails> runDetails = runDetailsList.stream().findFirst();
-
-
-        List<Map<String, Object>> entireValidationTable = new ArrayList<>();
-        if(runDetails.isPresent()) {
-            DatabaseInfo valTableDbInfo = new DatabaseInfo("localhost", 5432,
-                    "ttp", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                    true, "/certs/", "changeit");
-
-            String validationTableName = runDetails.get().getTableName() + "_val";
-            entireValidationTable = recommendationService.getEntireValidationTable(runDetails.get(), valTableDbInfo, validationTableName);
-        }
-        return entireValidationTable;
-
-    }
-
-    //http://localhost:8080/recommendation/api/source-target/recommendation
+    //http://localhost:8080/dvt/recommendation/recommendation-data
     // Pass below as post request body
     // ?sourceHostName=localhost&targetHostName=localhost&databaseName=ttp&schemaName=ops$ora&tableName=ppt12&schemaRun=1&tableRun=2
 
-    @PostMapping(path = "/source-target/recommendation",
+    @PostMapping(path = "/recommendation/recommendation-data",
                 consumes = MediaType.APPLICATION_JSON_VALUE,
                 produces = MediaType.APPLICATION_JSON_VALUE)
         RecommendationResponse getRecommendationResponse(@RequestBody RunDetails inputRunDetails) throws Exception {
@@ -244,92 +86,8 @@ public class RecommendationController {
 
         }
 
-        //http://localhost:8080/recommendation/api/run-details/source/val-id?sourceHostName=localhost&targetHostName=localhost&databaseName=ttp
-    @GetMapping("/run-details/source/val-id")
-    public List<Integer> getValIdFromValidationTable(
-            @RequestParam Optional<String> sourceHostName,
-            @RequestParam Optional<String> targetHostName,
-            @RequestParam Optional<String> databaseName,
-            @RequestParam Optional<String> schemaName,
-            @RequestParam Optional<String> tableName,
-            @RequestParam Optional<Integer> schemaRun,
-            @RequestParam Optional<Integer> tableRun
-    ) throws Exception {
 
-        RunDetails inputRunDetails = new RunDetails();
-        if (sourceHostName.isPresent()) {
-            inputRunDetails.setSourceHostName(sourceHostName.get());
-        }
-        if (targetHostName.isPresent()) {
-            inputRunDetails.setTargetHostName(targetHostName.get());
-        }
-        if (databaseName.isPresent()) {
-            inputRunDetails.setDatabaseName(databaseName.get());
-        }
-        if (schemaName.isPresent()) {
-            inputRunDetails.setSchemaName(schemaName.get());
-        }
-        if (tableName.isPresent()) {
-            inputRunDetails.setTableName(tableName.get());
-        }
-        if (schemaRun.isPresent()) {
-            inputRunDetails.setSchemaRun(schemaRun.get());
-        }
-        if (tableRun.isPresent()) {
-            inputRunDetails.setTableRun(tableRun.get());
-        }
-
-        DatabaseInfo runTableDbInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        List<RunDetails> runDetailsList = recommendationService.getRunDetailsWithOptional(inputRunDetails, runTableDbInfo);
-        Optional<RunDetails> runDetails = runDetailsList.stream().findFirst();
-
-
-        List<Integer> sourceValIdFromValidationTable = new ArrayList<>();
-        if(runDetails.isPresent()) {
-            DatabaseInfo valTableDbInfo = new DatabaseInfo("localhost", 5432,
-                    "ttp", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                    true, "/certs/", "changeit");
-
-            String validationTableName = runDetails.get().getTableName() + "_val";
-            sourceValIdFromValidationTable = recommendationService.getValIdFromValidationTable(runDetails.get(), valTableDbInfo, validationTableName);
-        }
-        return sourceValIdFromValidationTable;
-
-    }
-
-
-    //http://localhost:8080/recommendation/api/run-details
-    @GetMapping("/run-details-old")
-    public List<RunDetails> getRunDetails() throws Exception {
-
-        RunDetails inputRunDetails_1 = new RunDetails("localhost", "localhost", "postgres", "public", "company");
-
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        return recommendationService.getRunDetails(inputRunDetails_1, databaseInfo);
-    }
-
-
-    @GetMapping("/execute-db-procedure")
-    public boolean executeDbProcedure() throws Exception {
-
-        //RunDetails inputRunDetails_1 = new RunDetails("localhost","localhost","postgres","public","company",3);
-        RunDetails inputRunDetails_1 = new RunDetails("localhost", "localhost", "postgres", "public", "company");
-        //RunDetails inputRunDetails_1 = new RunDetails("localhost","localhost","postgres","public");
-
-        DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
-                "postgres", null, "postgres", "postgres", false, DatabaseInfo.dbType.POSTGRESQL,
-                true, "/certs/", "changeit");
-
-        return recommendationService.executeDbProcedure(inputRunDetails_1, databaseInfo);
-    }
-
-    @GetMapping("/insert-run-details-record")
+    @GetMapping("/remediation/remediate-data")
     public int insertRunDetailsRecord() throws Exception {
         RunDetails inputRunDetails_1 = new RunDetails("localhost", "localhost", "postgres", "public", "company", 5);
         DatabaseInfo databaseInfo = new DatabaseInfo("localhost", 5432,
@@ -339,7 +97,7 @@ public class RecommendationController {
         return recommendationService.insertRunDetailsRecord(inputRunDetails_1, databaseInfo);
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/recommendation/upload")
     public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file ) throws Exception {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         String timeStampStr=timestamp.toString();
