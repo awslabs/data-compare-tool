9f4e46a736439d4c95fd8ba5ce65e38fe550e299
Madhu Athinarapu <amsudan@amazon.com>
2023-03-24 12:40:46 +0530

    Integration changes for cognito

---
+++ app/src/routes/auth/changePassword.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useContext } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+
+import { useValidPassword } from '../../hooks/useAuthHooks'
+import { Password } from '../../components/authComponents'
+
+import { AuthContext } from '../../contexts/authContext'
+
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+  },
+})
+
+export default function ChangePassword() {
+  const classes = useStyles()
+
+  const [error, setError] = useState('')
+  const [reset, setReset] = useState(false)
+
+  const {
+    password: oldPassword,
+    setPassword: setOldPassword,
+    passwordIsValid: oldPasswordIsValid,
+  } = useValidPassword('')
+
+  const {
+    password: newPassword,
+    setPassword: setNewPassword,
+    passwordIsValid: newPasswordIsValid,
+  } = useValidPassword('')
+
+  const isValid = !oldPasswordIsValid || oldPassword.length === 0 || !newPasswordIsValid || newPassword.length === 0
+
+  const history = useNavigate()
+
+  const authContext = useContext(AuthContext)
+
+  const changePassword = async () => {
+    try {
+      await authContext.changePassword(oldPassword, newPassword)
+      setReset(true)
+    } catch (err: any) {
+      setError(err.message)
+    }
+  }
+
+  const signOut = async () => {
+    try {
+      await authContext.signOut()
+      history('/')
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message)
+      }
+    }
+  }
+
+  const updatePassword = (
+    <>
+      <Box width="80%" m={1}>
+        <Password label="Old Password" passwordIsValid={oldPasswordIsValid} setPassword={setOldPassword} />
+      </Box>
+      <Box width="80%" m={1}>
+        <Password label="Password" passwordIsValid={newPasswordIsValid} setPassword={setNewPassword} />
+      </Box>
+      {/* Error */}
+      <Box mt={2}>
+        <Typography color="error" variant="body2">
+          {error}
+        </Typography>
+      </Box>
+
+      {/* Buttons */}
+      <Box mt={2}>
+        <Grid container direction="row" justify="center">
+          <Box m={1}>
+            <Button onClick={() => history(-1)} color="secondary" variant="contained">
+              Cancel
+            </Button>
+          </Box>
+          <Box m={1}>
+            <Button disabled={isValid} color="primary" variant="contained" onClick={changePassword}>
+              Change Password
+            </Button>
+          </Box>
+        </Grid>
+      </Box>
+    </>
+  )
+
+  const passwordReset = (
+    <>
+      <Typography variant="h5">{`Password Changed`}</Typography>
+
+      <Box m={4}>
+        <Button onClick={signOut} color="primary" variant="contained">
+          Sign In
+        </Button>
+      </Box>
+    </>
+  )
+
+  return (
+    <Grid className={classes.root} container direction="row" justify="center" alignItems="center">
+      <Grid xs={11} sm={6} lg={4} container direction="row" justify="center" alignItems="center" item>
+        <Paper style={{ width: '100%', padding: 16 }}>
+          <Grid container direction="column" justify="center" alignItems="center">
+            {/* Title */}
+            <Box m={3}>
+              <Grid container direction="row" justify="center" alignItems="center">
+                <Typography variant="h3">Change Password</Typography>
+              </Grid>
+            </Box>
+
+            {!reset ? updatePassword : passwordReset}
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+}
---
+++ app/src/routes/home.tsx
@@ -0,0 +1,94 @@
+import React, { useContext } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import Link from '@material-ui/core/Link'
+
+import logoImage from './logo.png'
+
+import { AuthContext } from '../contexts/authContext'
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  title: {
+    textAlign: 'center',
+  },
+  session: {
+    width: '80vw',
+    overflow: 'auto',
+    overflowWrap: 'break-word',
+    fontSize: '16px',
+  },
+  hero: {
+    width: '100%',
+    background: 'rgb(220,220,220)',
+  },
+}))
+
+export default function Home() {
+  const classes = useStyles()
+
+  const history = useNavigate()
+
+  const auth = useContext(AuthContext)
+
+  function signOutClicked() {
+    auth.signOut()
+    history('/')
+  }
+
+  function changePasswordClicked() {
+    history('changepassword')
+  }
+
+  return (
+    <Grid container>
+      <Grid className={classes.root} container direction="column" justify="center" alignItems="center">
+        <Box className={classes.hero} p={4}>
+          <Grid className={classes.root} container direction="column" justify="center" alignItems="center">
+            <Box m={2}>
+              <img src={logoImage} width={224} height={224} alt="logo" />
+            </Box>
+            <Box m={2}>
+              <Link underline="none" color="inherit" href="https://github.com/dbroadhurst/aws-cognito-react">
+                <Grid container direction="row" justify="center" alignItems="center">
+                  <Box mr={3}>
+                    <GitHubIcon fontSize="large" />
+                  </Box>
+                  <Typography className={classes.title} variant="h3">
+                    AWS Cognito Starter Home
+                  </Typography>
+                </Grid>
+              </Link>
+            </Box>
+            <Box m={2}>
+              <Button onClick={signOutClicked} variant="contained" color="primary">
+                Sign Out
+              </Button>
+            </Box>
+            <Box m={2}>
+              <Button onClick={changePasswordClicked} variant="contained" color="primary">
+                Change Password
+              </Button>
+            </Box>
+          </Grid>
+        </Box>
+        <Box m={2}>
+          <Typography variant="h5">Session Info</Typography>
+          <pre className={classes.session}>{JSON.stringify(auth.sessionInfo, null, 2)}</pre>
+        </Box>
+        <Box m={2}>
+          <Typography variant="h5">User Attributes</Typography>
+          <pre className={classes.session}>{JSON.stringify(auth.attrInfo, null, 2)}</pre>
+        </Box>
+      </Grid>
+    </Grid>
+  )
+}
