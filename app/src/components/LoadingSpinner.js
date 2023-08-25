b4a0913c728d5f184ccdd1bbb1105b5b14b48c0f
Harsh Pandey <hupandey@amazon.com>
2023-08-25 15:36:41 +0530

    updated loader with DVT text

--- app/src/components/LoadingSpinner.js
+++ app/src/components/LoadingSpinner.js
@@ -10,10 +10,8 @@ export default function LoadingSpinner() {
       <div className="loading-container">
         <div className="loading">
           <div className="loading-text">
-            <span className="loading-text-words">S</span>
-            <span className="loading-text-words">C</span>
-            <span className="loading-text-words">O</span>
-            <span className="loading-text-words">O</span>
+            <span className="loading-text-words">D</span>
+            <span className="loading-text-words">V</span>
             <span className="loading-text-words">T</span>
             <div className="is-loading-text">is loading...</div>
           </div>
